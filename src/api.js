36f04c160b6b0fdb800fd6277a93d8df04da4a80
minzz1 <hohohohohoh52@gmail.com>
2023-08-07 13:56:06 +0900

    휴 ...

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const BASE_PATH = process.env.NODE_ENV === "production" ? "https://port-0-yj-react-pj4-back-ac2nll079vut.sel4.cloudtype.app" : "http://localhost:8080";
-
+// const BASE_PATH = process.env.NODE_ENV === "production" ? "https://port-0-yj-react-pj4-back-ac2nll079vut.sel4.cloudtype.app" : "http://localhost:8080";
+const BASE_PATH = process.env.NODE_ENV === "production" ? "https://wondrous-semifreddo-7da894.netlify.app" : "http://localhost:8080";
 export async function rentalNotices() {
   return await fetch(`${BASE_PATH}/api/rental/notice`, {
     method: "GET",
